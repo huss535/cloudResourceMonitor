3f62fb4e8f70b4d646973a5b8d2bd5d8652f7d90
efar716 <110000165+efar716@users.noreply.github.com>
2025-02-03 13:12:49 +1300

    final push

--- lambda/awsResourcesLambda.py
+++ lambda/awsResourcesLambda.py
@@ -3,31 +3,28 @@ import os
 import logging
 from discord_webhook import DiscordWebhook
 
-# Set up logging for debugging through cloudwatch
+# Set up logging for debugging through CloudWatch
 logger = logging.getLogger()
-logger.setLevel(logging.INFO) 
+logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
     try:
-       #log incoming package for debugging
+        # Log incoming event for debugging
         logger.info(f"Received event: {json.dumps(event, indent=2)}")
 
-      
-        
-
-      
-        
-        event_account = event.get("account", "could not retrieve account") # account performing aws resource changes
-        event_type = event.get("detail-type", "could not retrieve event type") # triggered event type
-        event_service = event.get("source", "could not retrieve service")  # AWS service modified i.e. S3
-        event_resources = event.get("detail", {})# Modified resource information i.e. S3 Bucket
+        # Extract event details
+        event_account = event.get("account", "could not retrieve account")  # Account performing AWS resource changes
+        event_type = event.get("detail-type", "could not retrieve event type")  # Triggered event type
+        event_service = event.get("source", "could not retrieve service")  # AWS service modified (e.g., S3)
+        event_resources = event.get("detail", {})  # Modified resource information (e.g., S3 Bucket)
 
         # Construct the Discord message
         message = (
-        f"**Event Type:** {event_type}\n"
-        f"**User Account ID:** {event_account}\n"
-        f"**Service Used:** {event_service}\n"
-        f"**Resource State:**\n```json\n{json.dumps(event_resources, indent=2)}\n```"
+            f"**Event Type:** {event_type}\n"
+            f"**User Account ID:** {event_account}\n"
+            f"**Service Used:** {event_service}\n"
+            f"**Resource State:**\n```json\n{json.dumps(event_resources, indent=2)}\n```"
         )
 
         # Send the message to Discord
